113e0864d8edf7b96f4849f593811bcf538b5b22
Oksana Danilova <ilimlaw38@gmail.com>
2024-11-05 18:55:08 +0800

    fixed bugs

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -1,29 +1,9 @@
 import Character from '../character';
 
-test('should check the class Character no name < 2', () => {
-  expect(() => new Character('a', 'Bowman')).toThrowError(new Error('Name is short or long'));
+test('checking the length of the name', () => {
+  expect(() => new Character('a', 'Bowman')).toThrow(new Error('Name is short or long'));
 });
 
-test('should check the class Character no name', () => {
-  expect(() => new Character('Player', 'Hero')).toThrowError(new Error('Wrong type'));
-});
-
-test('should check the class Character no name', () => {
-  const character = new Character('Player', 'Magician');
-  expect(character.name).toBe('Player');
-});
-
-test('should check the class Character no name', () => {
-  const character = new Character('Player', 'Magician');
-  expect(character.type).toBe('Magician');
-});
-test('should check the class Character no name', () => {
-  const character = new Character('Player', 'Magician');
-  const result = {
-    name: 'Player',
-    type: 'Magician',
-    health: 100,
-    level: 1,
-  };
-  expect(character).toEqual(result);
+test('type checking', () => {
+  expect(() => new Character('Player', 'Hero')).toThrow(new Error('Wrong type'));
 });
